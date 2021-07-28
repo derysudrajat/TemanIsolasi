4b70a89f31d9233c4ae87ae1aedaca188db467b8
derysudrajat <dery.sudrajat17@gmail.com>
2021-07-28 13:38:57 +0700

    add: Loading

--- app/src/main/java/id/temanisolasi/ui/register/RegisterViewModel.kt
+++ app/src/main/java/id/temanisolasi/ui/register/RegisterViewModel.kt
@@ -8,10 +8,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import id.temanisolasi.R
 import id.temanisolasi.data.model.User
 import id.temanisolasi.data.repo.State
 import id.temanisolasi.data.repo.remote.firebase.auth.AuthRepository
 import id.temanisolasi.data.repo.remote.firebase.firestore.FirestoreUserRepository
+import id.temanisolasi.utils.DialogHelpers
 import id.temanisolasi.utils.Helpers
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -33,20 +35,21 @@ class RegisterViewModel(
     fun createAccount(
         user: User, password: String, activity: Activity, isSuccess: () -> Unit
     ) = viewModelScope.launch {
+        val dialog = DialogHelpers(activity)
+        dialog.init(R.layout.dialog_base)
         user.email?.let { mail ->
             authRepository.signUpWithEmail(mail, password).collect {
                 when (it) {
-                    is State.Loading -> {
-                    }
+                    is State.Loading -> dialog.showDialog()
                     is State.Success -> it.data.let { map ->
                         Log.d(
                             "TAG",
                             "isSuccess ${map.keys}: ${map.values}, id = ${Firebase.auth.currentUser?.uid}"
                         )
-                        recordDataUser(user, activity) { isSuccess() }
+                        recordDataUser(user, activity, dialog) { isSuccess() }
                     }
                     is State.Failed -> {
-//                        DialogHelpers.hideLoadingDialog()
+                        dialog.hideDialog()
                         Helpers.showToast(activity, it.message)
                     }
                 }
@@ -55,21 +58,33 @@ class RegisterViewModel(
 
     }
 
-    fun recordDataUser(user: User, activity: Activity, isSuccess: () -> Unit) =
-        viewModelScope.launch {
-            userRepository.createNewUser(user).collect {
-                when (it) {
-                    is State.Loading -> Log.d("TAG", "loading: true")
-                    is State.Success -> {
-//                        DialogHelpers.hideLoadingDialog()
-                        isSuccess()
-                    }
-                    is State.Failed -> {
-//                        DialogHelpers.hideLoadingDialog()
-                        Helpers.showToast(activity, it.message)
-                    }
+    fun recordDataUser(
+        user: User,
+        activity: Activity,
+        dialog: DialogHelpers? = null,
+        isSuccess: () -> Unit
+    ) = viewModelScope.launch {
+        val mDialog: DialogHelpers?
+        if (dialog == null) {
+            mDialog = DialogHelpers(activity)
+            mDialog.init(R.layout.dialog_base)
+        } else mDialog = dialog
+        userRepository.createNewUser(user).collect {
+            when (it) {
+                is State.Loading -> {
+                    if (dialog == null) mDialog.showDialog()
+                    Log.d("TAG", "loading: true")
+                }
+                is State.Success -> {
+                    mDialog.hideDialog()
+                    isSuccess()
+                }
+                is State.Failed -> {
+                    mDialog.hideDialog()
+                    Helpers.showToast(activity, it.message)
                 }
             }
         }
+    }
 
 }
